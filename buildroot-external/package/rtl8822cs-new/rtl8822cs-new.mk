b4ae40cfdaf36ef51f86a1e89adfba4d2e78fc0a
Viacheslav Bocharov <adeep@lexina.in>
2025-09-22 13:41:25 +0300

    update package/rtl8822cs-new to latest (6.16+ kernel support)

--- buildroot-external/package/rtl8822cs-new/rtl8822cs-new.mk
+++ buildroot-external/package/rtl8822cs-new/rtl8822cs-new.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-RTL8822CS_NEW_VERSION = 0986d5734e7630efea218dae9c16d11a10231819
-RTL8822CS_NEW_SITE = $(call github,jethome-ru,rtl88x2cs,$(RTL8822CS_NEW_VERSION))
+RTL8822CS_NEW_VERSION = 0ef9ddd619d2a386df90fd7c32b65958b0d675ed
+RTL8822CS_NEW_SITE = $(call github,jethome-iot,rtl88x2cs,$(RTL8822CS_NEW_VERSION))
 RTL8822CS_NEW_LICENSE = GPL-2.0
 
 RTL8822CS_NEW_MODULE_MAKE_OPTS = \
